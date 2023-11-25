fe4d18db45dc3a3cc939c18dcf4a50a255c92d57
Hugos68 <hugokorteapple@gmail.com>
2023-11-25 22:01:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# svelte-broadcastable
+
+Svelte broadcastable is a store wrapper for the [Broadcast API](https://developer.mozilla.org/en-US/docs/Web/API/Broadcast_Channel_API). It syncs any data across a given channel.
+
+## Usage
+
+You can import the broadcastable store like so:
+
+```js
+import { broadcastable } from 'svelte-broadcastable';
+```
+
+You can use the store like this:
+
+```js
+const init = 'hello';
+
+const store = broadcastable('my-channel', init);
+```
+
+The API is identical to the svelte `writable` store except that it adds 1 parameter called `name`, this will be the name that is used to instantiate the BroadcastChannel, this allows for using multiple channels when sharing state.
---
+++ src/lib/index.ts
@@ -0,0 +1,41 @@
+import { writable, type Updater, type StartStopNotifier } from 'svelte/store';
+
+export function broadcastable<T>(name: string, value?: T, start: StartStopNotifier<T> = () => {}) {
+	const channel = new BroadcastChannel(name);
+
+	const {
+		subscribe: _subscribe,
+		set: _set,
+		update: _update
+	} = writable<T>(value, (set, update) => {
+		const stop = start(set, update);
+		function messageHandler({ data }: MessageEvent<T>) {
+			set(data);
+		}
+		channel.addEventListener('message', messageHandler);
+		return () => {
+			channel.removeEventListener('message', messageHandler);
+			channel.close();
+			if (stop) stop();
+		};
+	});
+
+	function update(updater: Updater<T>) {
+		_update((value) => {
+			const newValue = updater(value);
+			channel.postMessage(newValue);
+			return newValue;
+		});
+	}
+
+	function set(value: T) {
+		channel.postMessage(value);
+		_set(value);
+	}
+
+	return {
+		subscribe: _subscribe,
+		set,
+		update
+	};
+}
